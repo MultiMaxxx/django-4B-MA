cb2b53e48f7a0cc9bb5a523c2c92d2895e8542f9
MultiMaxxx <maks.andrus2014@gmail.com>
2024-04-25 06:23:38 +0200

    123

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,13 +1,15 @@
 import datetime
+
 from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-    def __str__(self):
+
+    def __str__(self) -> str:
         return self.question_text
-        
+    
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
@@ -16,13 +18,14 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
 
+    def __str__(self) -> str:
+        return self.choice_text
+    
 def was_published_recently(self):
     now = timezone.now()
     return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    
+
 def test_was_published_recently_with_old_question(self):
     """
     was_published_recently() returns False for questions whose pub_date
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,30 +1,29 @@
+from django.utils import timezone
+from datetime import *
 from django.db.models import F
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+
 from django.views import generic
+from django.http import Http404
+from django.template import loader
+from .models import Question, Choice
 
-from .models import Choice, Question
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
-
-class IndexView(generic.ListView):
-    template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = "polls/detail.html"
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = "polls/results.html"
-    
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -46,3 +45,33 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(pub_date__lte=datetime.now()).order_by("-pub_date")[
+        :5
+    ]
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=datetime.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
